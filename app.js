c15aa3e0ee0c115aa64a11812bb94de617d14912
Aman3007 <amanchauhan30072002@gmail.com>
2025-06-10 13:20:38 -0500

    update repo

--- app.js
+++ app.js
@@ -4,22 +4,55 @@
  const app=express();
  app.set("view engine","ejs")
  app.use(express.static("public"))
- app.use(express.urlencoded({extended:true}))
+ app.use(bodyparser.urlencoded({extended:true}))
  
 //  var example="working";
  var items =[];
+ var idCounter=1;
 app.get("/",(req,res)=>{
     res.render("list",{exej:items})
 })
-app.post("/",(req,res)=>{
-
-    var item=req.body.todo;
-    console.log(item)
-         items.push(item);
-    res.redirect("/")
-    
-   
+app.post("/add",(req,res)=>{
+    const {todo,priority}=req.body;
+     if (todo.trim() === "") 
+        {return res.redirect("/");}
+     else{
+         items.push({ id: idCounter++, todo, priority });
+        res.redirect("/");
+     }
+ 
 })
+
+app.get("/filter", (req, res) => {
+  const filter = req.query.priority || "";
+  const filteredTodos = filter
+    ? items.filter((todo) => todo.priority === filter)
+    : items;
+  res.render("list", { exej: filteredTodos });
+});
+
+app.get("/edit/:id", (req, res) => {
+  const todo = items.find((t) => t.id === parseInt(req.params.id));
+  res.render("edit", { task:todo });
+});
+
+app.post("/update/:id", (req, res) => {
+  const { task, priority } = req.body;
+  const todow = items.find((t) => t.id === parseInt(req.params.id));
+  if (todow) {
+    todow.todo = task;
+    todow.priority = priority;
+  }
+  res.redirect("/");
+});
+
+app.post("/delete/:id", (req, res) => {
+  items = items.filter((t) => t.id !== parseInt(req.params.id));
+  res.redirect("/");
+});
+
+
+
  app.listen(3000,()=>{
     console.log("server started")
  })
